b0d6df0b11392c8f846782f17bde78419946f88d
wthew <wellingtondeferreiraqueiroz@gmail.com>
2021-09-20 11:30:49 -0300

    init now works!

--- viper/__init__.py
+++ viper/__init__.py
@@ -4,13 +4,11 @@ from .init import main as __init
 from .install import main as __install
 from .remove import main as __remove
 
-
 operations = {
-    "install": lambda pos_args: __install(pos_args),
-    "remove": lambda pos_args: __remove(pos_args),
-    "init": lambda pos_args: __init(pos_args),
+    "install": __install,
+    "remove": __remove,
+    "init": __init,
 }
-
 
 def main(args: Namespace):
     operation = args.operation
--- viper/init.py
+++ viper/init.py
@@ -4,19 +4,42 @@ import yaml
 
 def main(args: list[str]):
     user_location = os.getcwd()
-    user_name = os.environ["USER"]
     project_yaml_path = os.path.join(user_location, "package.yaml")
 
-    print("\ninit a project in", user_location)
-
     if os.path.isfile(project_yaml_path):
-        print("there is a package.yaml... aborting")
+        print("there is a package.yaml here already... aborting")
         return
 
     project_name = input("Name of project: ") if len(args) == 0 else args[0]
+    user_name = os.environ["USER"] or input("What is your name? ")
 
     project_dict = mount_project_dictonary(project_name, user_name)
-    project_yaml = yaml.dump(project_dict)
+    project_yaml = yaml.dump(project_dict, sort_keys=False)
+
+    print(f"\n🔧 Making {project_name}, an awesome project, in", user_location)
+
+    os.mkdir(project_name)
+    os.chdir(project_name)
+
+    with open("package.yaml", "+w") as file:
+        file.write(project_yaml)
+
+    print("\n🐍 creating venv...", end="\r")
+
+    os.system("python -m venv venv")
+
+    print("🐍 creating venv... created!")
+
+    print("\nMaking initials files...")
+
+    os.mkdir(project_name)
+    os.system(f"touch {project_name}/__init__.py")
+
+    print("\n🐙 creating git local repo...")
+    os.system("git init")
+    os.system('echo "venv" >> .gitignore')
+
+    print(f"\n✨ Done. now, open with: `code {project_name}`")
 
 
 def mount_project_dictonary(name, author):
