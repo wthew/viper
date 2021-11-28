bbdb7b3a4872278be86ab096f95615afed70df00
wthew <wellingtondeferreiraqueiroz@gmail.com>
2021-11-28 18:05:09 -0300

    fix: args in cli

--- viper/__main__.py
+++ viper/__main__.py
@@ -1,14 +1,17 @@
 from argparse import ArgumentParser
-from . import main, operations
+from . import main as run, operations
 
 # args parser
+def main():
+    parser = ArgumentParser(prog="Viper", description="A yarn like Python Package Manager")
 
-parser = ArgumentParser(prog="Viper", description="A yarn like Python Package Manager")
+    parser.add_argument("operation", choices=operations.keys())
 
-parser.add_argument("operation", choices=operations.keys())
+    parser.add_argument("package", nargs='*', help="What package?")
 
-parser.add_argument("package", nargs='*', help="What package?")
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    run(args)
 
-main(args)
+if __name__ == "__main__":
+    main()
